ba5ce0eb476459f733c4aeea775c419a93f0c5bb
unknown <surmaannalo@gmail.com>
2021-11-03 19:08:13 +0100

    first commit

---
+++ PierwszeZadanie/src/PierwszeZadanie.java
@@ -0,0 +1,16 @@
+public class PierwszeZadanie {
+    public static void main(String[] args) {
+        System.out.println("Cześć Jan, słyszałem, że masz 25 lat i uczysz się programowania. Czy to prawda?");
+        System.out.println(true);
+        System.out.println("Czy Twoje inicjały to J K?");
+        System.out.println("tak" +  true);
+
+
+        System.out.println("Mam Audi A4");
+        System.out.println("Rocznik:");
+        System.out.println(2004);
+        System.out.println("Silnik o pojemności:");
+        System.out.println(2.0);
+
+    }
+}
---
+++ Zmienne/src/Zmienne.java
@@ -0,0 +1,14 @@
+public class Zmienne {
+    public static void main(String[] args) {
+        String marka = "volvo";
+        String model = "Superior";
+        int rocznik = 2000;
+        double cena = 24.999;
+        System.out.println("Nazwa marki samochodu" + " " + marka);
+        System.out.println("Nazwa modelu samochodu" + " " + model);
+        System.out.println("Rocznik samochodu" + " " + rocznik);
+        System.out.println("Cena samochodu"+ " " + cena);
+
+
+    }
+}
